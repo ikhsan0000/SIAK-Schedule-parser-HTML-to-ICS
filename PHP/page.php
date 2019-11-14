bd8d33a07f1f04241809371faec431d0991e4ff9
ikhsan0000 <46219884+ikhsan0000@users.noreply.github.com>
2019-11-14 12:58:31 +0700

    Create page.php

---
+++ PHP/page.php
@@ -0,0 +1,19 @@
+<html>
+  
+  <head>
+		<title> SchedUIe </title>
+	</head>
+	
+  <body>
+	<h1 align='middle'> SchedUIe </h1>
+	<form action="convert.php" method="post" enctype="multipart/form-data">
+    File HTML siak:
+    <input type="file" name="jadwalsiak" id="fileToUpload"><br>
+	<input type="radio" name="radio" value="Radio 1">Mahasiswa 
+	<input type="radio" name="radio" value="Radio 1">Dosen<br> 
+	<input type="submit" value="Submit" name="submit"> 
+	</form>
+
+</body>
+</html>
+
