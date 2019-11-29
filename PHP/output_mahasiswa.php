f3642877d833ac41440c52d62c45f2dc86d6d296
ikhsan0000 <46219884+ikhsan0000@users.noreply.github.com>
2019-11-30 00:59:12 +0700

    Update output_mahasiswa.php

--- PHP/output_mahasiswa.php
+++ PHP/output_mahasiswa.php
@@ -318,21 +318,7 @@
 			$inc_hari = $inc_hari + 1;
 		}
 		fwrite($handle,	'END:VCALENDAR');
-		/*if (file_exists($file)) 
-		{
-		  header('Content-Description: File Transfer');
-			header('Content-Type: application/octet-stream');
-			header('Content-Disposition: attachment; filename='.basename($file));
-			header('Content-Transfer-Encoding: binary');
-			header('Expires: 0');
-			header('Cache-Control: must-revalidate');
-			header('Pragma: public');
-			header('Content-Length: ' . filesize($file));
-			ob_clean();
-			flush();
-			readfile($file);
-			exit;
-		}*/
+		
 	}
 	
 	
