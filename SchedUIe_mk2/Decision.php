07958d5740eaf767515136af17cdff32f2fb6a6f
ikhsan0000 <46219884+ikhsan0000@users.noreply.github.com>
2019-12-08 11:01:33 +0700

    Update Decision.php

--- SchedUIe_mk2/Decision.php
+++ SchedUIe_mk2/Decision.php
@@ -1,37 +1,29 @@
 <?php	
 $role = '';
   
-  if (empty($_POST["role"]))
-  {
-	  $role_error = "Role is required";
-  }
-  else
-  {
-	  $role = test_input($_POST["role"]);
-  }
-  
-  function test_input($data) 
-  {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-  }
 
-	if (isset($role) && $role == "mahasiswa")
+	if (isset($_POST['role']) && $_POST['role'] == "mahasiswa")
 	{
 		include_once('output_mahasiswa.php');
 	}
-	elseif (isset($role) && $role == "dosen")
+	elseif (isset($_POST['role']) && $_POST['role'] == "dosen")
 	{
 		include_once('output_dosen.php');
 	}
 	else
 	{
 		echo '<script language="javascript">';
-		echo 'alert("Please select your role")';
-		echo 'window.location.replace("home.php")';
+		echo 'alert("Please select your role");';
+		echo 'window.location="home.php";';
 		echo '</script>';
 		exit();
 	}
 ?>
+
+<html>
+<head>
+</head>
+<body>
+
+</body>
+</html>
