d659dbc797f3d6ee0d7cbfde194fe5589e28623b
GalihDJ <44154895+GalihDJ@users.noreply.github.com>
2020-04-07 20:17:35 +0700

    Add files via upload

--- SchedUIe_+_Event/event.php
+++ SchedUIe_+_Event/event.php
@@ -7,6 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="description" content="Your page description here" />
   <meta name="IGS" content="" />
+
+
   <!-- css -->
   <link href="css/bootstrap.css" rel="stylesheet" />
   <link href="css/bootstrap-responsive.css" rel="stylesheet" />
@@ -20,11 +22,14 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Theme skin -->
   <link id="t-colors" href="color/default.css" rel="stylesheet" />
+
   <!-- Fav and touch icons -->
   <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png" />
   <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png" />
   <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png" />
   <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png" />
+
+
   <!-- =======================================================
     Theme Name: Remember
     Theme URL: https://bootstrapmade.com/remember-free-multipurpose-bootstrap-template/
@@ -32,7 +37,9 @@
     Author URL: https://bootstrapmade.com
   ======================================================= -->
 </head>
+
 <body>
+
   <div id="wrapper">
     <!-- start header -->
     <header>
@@ -44,12 +51,12 @@
                 <li><a href="home.php">Home</a>&nbsp;&nbsp;</li>
 				<li><a href=#intro>Introduction</a>&nbsp;&nbsp;</li>
 				<li><a href=#content>Manual</a>&nbsp;&nbsp;</li>
-				<li><a href=event.php>Iklankan Acaramu</a>&nbsp;&nbsp;</li>
 				<li><a href=#mainprog>Main Program</a>&nbsp;&nbsp;</li>
 				<li><a href=#profile>About</a></li>
               </ul>
             </div>
             <div class="span6">
+
             </div>
           </div>
         </div>
@@ -59,7 +66,7 @@
           <div class="span4">
             <div class="logo">
               <h1>
-                <a href="home.php">Sched<span style="color:yellow">UI</span>e </a>+
+                <a href="home.php" target="_blank">Sched<span style="color:yellow">UI</span>e </a>+
                 <a href="event.php">Event</a>
               </h1>
             </div>
@@ -75,32 +82,36 @@
       </div>
     </header>
     <!-- end header -->
+
     <!-- section intro -->
     <section id="intro">
       <div class="intro-content">
+
         <h2>Event<span style="color:yellow"> UI</span></h2>
         <h3>Mau event-mu lebih ramai? SchedUIe bisa mengirimkan email pemberitahuan event untuk mengundang beberapa potential visitor ke event-mu</h3>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/4g2m41EbLgU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		<form action ="insert_acara.php" method='post'>
+
+        <form>
      <div class="form-group input-group-lg">
-         <label for="fname"><font color="white">Organisasi:</font></label><br>
+         <label for="fname" style="color:white">Organisasi:</label><br>
          <input type="text" id="org" name="org" class="form-control form-control-lg"><br>
      </div>
      <div class="form-group input-group-lg">
-         <label for="lname"><font color="white">Nama acara:</font></label><br>
+         <label for="lname" style="color:white">Nama acara:</label><br>
          <input type="text" id="e_name" name="e_name" class="form-control form-control-lg">
         </div>
-     <div class="form-group input-group-lg">
-         <label for="e_desc"><font color="white">Deskripsi acara:</font></label>
-		 <input type="text" name="e_desc" class="form-control form-control-lg>
+     <div class="form-group">
+         <label for="e_desc" style="color:white">Deskripsi acara:</label><br>
+         <textarea id="e_desc" name="e_desc" placeholder="Deskripsikan acaranya.." style="height:200px"></textarea>
          </div>
      <div class="form-group input-group-lg">
-		 <label for="fname"><font color="white">Tanggal:</font></label><br>
-		 <input type="text" id="e_date" name="e_date" class="form-control form-control-lg"><br>
+     <label for="e_date" style="color:white">Tanggal:</label><br>
+         <input type="text" id="e_date" name="e_date" class="form-control form-control-lg"><br>
+         <label for="e_day" style="color:white">Hari:</label><br>
         </div>
      <div class="form-group input-group-lg">
-	 <label for="lname"><font color="white">Hari:</font></label><br>
-     <select id="hari" name="hari" placeholder='Pilih Hari' class="form-control form-control-lg">
+     <select id="hari" name="hari" class="form-control form-control-lg">
+         <option hidden="">Pilih Hari</option>
          <option value="senin">Senin</option>
          <option value="selasa">Selasa</option>
          <option value="rabu">Rabu</option>
@@ -111,15 +122,19 @@
          </select>
      </div>
      <div class="form-group input-group-lg">
-         <label for="lname"><font color="white">Waktu mulai:</font></label><br>
+         <label for="e_start" style="color:white">Waktu mulai:</label><br>
          <input type="text" id="start_time" name="start_time" class="form-control form-control-lg"><br>
         </div>
      <div class="form-group input-group-lg">
-      <label for="lname"><font color="white">Waktu selesai:</font></label><br>
+      <label for="e_end" style="color:white">Waktu selesai:</label><br>
          <input type="text" id="end_time" name="end_time" class="form-control form-control-lg">
      </div>
-         <center><input type="submit" class="btn btn-primary btn-dark btn-lg" value="Submit"></center>
+         <center><input type="submit" class="btn btn-primary btn-lg" value="Submit"></center>
        </form>
+
+
+
+
 	  </section>
 	<!-- /section intro -->
     <section id="content">
@@ -131,6 +146,8 @@
 		  </div>
         </div>
 	  </section>
+
+
 	          <!-- divider -->
         <div class="row">
           <div class="span12">
@@ -156,6 +173,7 @@
                 <p>
                   <a href="https://id.linkedin.com/in/ikhsan-firdauz-562a50147" target="_blank"><i class="icon-linkedin icon-circled icon-bglight active"></i></a>
                 </p>
+
               </div>
             </div>
           </div>
@@ -184,6 +202,9 @@
 		</div>
 	   </div>
     </section>
+
+
+
     <footer>
       <div class="container">
         <div class="row">
@@ -195,15 +216,17 @@
               <address>
 							  <strong>IGS</strong><br>
   							Teknik Komputer Universitas Indonesia 2017<br>
-  							RPL 02-2019
 			  </address>
               <p>For more information and support please contact:<br>
                 <i class="icon-envelope-alt"></i> scheduiebyigs@gmail.com
               </p>
             </div>
           </div>
+
+
             </div>
           </div>
+
       <div id="sub-footer">
         <div class="container">
           <div class="row">
@@ -211,6 +234,9 @@
               <div class="copyright">
                 <p><span>&copy; Remember Inc. All right reserved</span></p>
                 <p><span>Report Card icon by <a href="https://icons8.com/icon/13184/report-card">Icons8</a></span></p>
+
+
+
             <div class="span6">
               <div class="credits">
                 <!--
@@ -228,9 +254,11 @@
       		</div>
 		  </div>
 		</div>
+
     </footer>
   </div>
   <a href="#" class="scrollup"><i class="icon-angle-up icon-rounded icon-bglight icon-2x"></i></a>
+
   <!-- javascript
     ================================================== -->
   <!-- Placed at the end of the document so the pages load faster -->
@@ -245,6 +273,8 @@
   <script src="js/portfolio/setting.js"></script>
   <script src="js/animate.js"></script>
   <script src="js/custom.js"></script>
+
+
   <script>
 	// Add the following code if you want the name of the file appear on select
 	$(".custom-file-input").on("change", function() {
@@ -252,5 +282,7 @@
 	$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
 	});
 	</script>
+
 </body>
+
 </html>
