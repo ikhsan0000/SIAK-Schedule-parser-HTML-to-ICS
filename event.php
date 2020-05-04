989ba1b8efb96f6bd5f3586bc517e5e648726637
GalihDJ <44154895+GalihDJ@users.noreply.github.com>
2020-05-04 20:12:43 +0700

    Add files via upload
    4-5-2020 Very small UI adjustment

--- event.php
+++ event.php
@@ -51,7 +51,7 @@
                 <li><a href="home.php">Home</a>&nbsp;&nbsp;</li>
 				<li><a href=#intro>Introduction</a>&nbsp;&nbsp;</li>
 				<li><a href=#content>Manual</a>&nbsp;&nbsp;</li>
-				<li><a href=#mainprog>Main Program</a>&nbsp;&nbsp;</li>
+				<li><a href=#org_form>Main Program</a>&nbsp;&nbsp;</li>
 				<li><a href=#profile>About</a></li>
               </ul>
             </div>
@@ -91,8 +91,8 @@
         <h3>Mau event-mu lebih ramai? SchedUIe bisa mengirimkan email pemberitahuan event untuk mengundang beberapa potential visitor ke event-mu</h3>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/4g2m41EbLgU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <form>
-     <div class="form-group input-group-lg">
+        <form><br>
+     <div class="form-group input-group-lg" id="org_form">
          <label for="fname" style="color:white">Organisasi:</label><br>
          <input type="text" id="org" name="org" class="form-control form-control-lg"><br>
      </div>
