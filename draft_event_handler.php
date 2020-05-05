b19af04484fc13615e7310e8cd2681ccb35cb400
ikhsan0000 <46219884+ikhsan0000@users.noreply.github.com>
2020-05-05 19:41:41 +0700

    Update draft_event_handler.php

--- draft_event_handler.php
+++ draft_event_handler.php
@@ -32,7 +32,7 @@ function sendMail($recipient, $user_name, $event_name, $day, $date, $start, $end
 	$mail->Port = '587';
 	$mail->isHTML();
 	$mail->Username = 'scheduiebyigs@gmail.com';
-	$mail->Password = 'tekkom2017';
+	$mail->Password = '*********'; //change this to our password
 	$mail->SetFrom('scheduiebyigs-no_reply@gmail.com');
 	$mail->Subject = 'Hello World!!';
 	$mail->Body = $content;
