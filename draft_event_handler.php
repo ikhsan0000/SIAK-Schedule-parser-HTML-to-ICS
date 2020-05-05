38b8454f01ce51fa0293753fb378d859be32c2c5
ikhsan0000 <46219884+ikhsan0000@users.noreply.github.com>
2020-05-05 19:41:00 +0700

    Update draft_event_handler.php

--- draft_event_handler.php
+++ draft_event_handler.php
@@ -1,46 +1,89 @@
 <?php
 /* Attempt to connect to database */
 $link = pg_connect("host=localhost dbname=scheduie user=postgres password=120399");
-// Check connection
+//check connection
 if($link === false){
     die("ERROR: Could not connect. " . "Error");
 
 }
+	require_once('phpmailer/PHPMailerAutoload.php');
+//Functoin PHPMailer
+//source: https://github.com/PHPMailer/PHPMailer/tree/5.2-stable
+function sendMail($recipient, $user_name, $event_name, $day, $date, $start, $end)
+{
 
+
+	$content = "Halo $user_name\n
+				Kami dari SchedUIe mengajak antum untuk hadir dalam \n\n
+				Acara : $event_name\n
+				Hari : $day\n
+				Tanggal : $date\n
+				Pukul : $start - $end\n\n
+				Tiada kesan tanpa kehadiranmu kawan!\n
+				Salam --IGS";
 	
-	$kueri = "SELECT a.id, b.nama, a.hari, a.waktu_mulai, a.waktu_selesai FROM jadwal a RIGHT JOIN user_list b ON a.id = b.id";
-	$kueri2 = pg_query($kueri)or die (error);
-	while ($row = pg_fetch_assoc($kueri2))
-	{
-		if($row['id'] == 1706985981)
-		{
-			echo $row['id'];
-			echo "<br>";
-		}
-	}
-	
-	echo "<br>";
-	echo "<br>";
-	echo "<br>";
-	
+	$mail = new PHPMailer();
+	$mail->isSMTP();
+	$mail->SMTPDebug = 3;
+	$mail->SMTPAuth = true;
+	$mail->SMPTSecure = 'ssl';
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Mailer   = "smtp";
+	$mail->Port = '587';
+	$mail->isHTML();
+	$mail->Username = 'scheduiebyigs@gmail.com';
+	$mail->Password = 'tekkom2017';
+	$mail->SetFrom('scheduiebyigs-no_reply@gmail.com');
+	$mail->Subject = 'Hello World!!';
+	$mail->Body = $content;
+	$mail->AddAddress($recipient);
+
+
+	$mail->Send();
+	echo "mail sent!";
+
+}
+
+
 	$kueri_row = "SELECT * FROM user_list";
-	$count_row = pg_query("SELECT COUNT(1) FROM user_list");
-	$total_row = pg_fetch_row($count_row);
-	$total_row = $total_row[0];
 	$execute = pg_query($kueri_row);
 	
-
-	echo $total_row;
-	echo "<br>";
-	
+	//Loop trough all events in table acara (SELECT * FROM acara)
 	$kueri_e = "SELECT * FROM acara";
 	$execute_e = pg_query($kueri_e);
 	while($row_e = pg_fetch_assoc($execute_e))
 	{
 		$hari_event = $row_e['hari'];
+		//day of current event
 		$nama_event = $row_e['nama_acara'];
+		//name of current event
 		$e_mulai = $row_e['waktu_mulai'];
+		//start time of current event
 		$e_selesai = $row_e['waktu_selesai'];
+		//end time of current event
+		$tanggal_event = $row_e['tanggal'];
+		
+		
+		//block to fix time format (ex: from 1800 to 18:00)
+		//fix start event time
+		if(strlen($e_mulai) == 3)
+		{
+			$e_mulai_fixed = "0" . $e_mulai;
+		}
+		$e_mulai_fixed = str_split($e_mulai,2);
+		$e_mulai_final = $e_mulai[0] . ":" . $e_mulai[1];
+		
+		//fix end event time
+		if(strlen($e_selesai) == 3)
+		{
+			$e_selesai_fixed = "0" . $e_selesai;
+		}
+		$e_selesai_fixed = str_split($e_selesai,2);
+		$e_selesai_final = $e_selesai[0] . ":" . $e_selesai[1];
+		
+		
+		
+		//test block
 		echo "<br>";
 		echo $nama_event;
 		echo "<br>";
@@ -51,70 +94,118 @@ if($link === false){
 		echo $e_selesai;
 		echo "<br>";
 		echo "<br>";
+		//test block
 		
+		//Loop trough all users in table user_list (SELECT * FROM user_list) for all Events
+
 		while($row2 = pg_fetch_assoc($execute))
 		{
-			//if($row2['id'] == 1706043046)
-			//{
-				$current_user = $row2['id'];
-				$current_name = $row2['nama'];
-				$kueri_row2 = "SELECT a.id, b.nama, b.email, a.hari, a.waktu_mulai, a.waktu_selesai FROM jadwal a RIGHT JOIN user_list b ON a.id = b.id WHERE a.id = '$current_user'";
-				$execute2 = pg_query($kueri_row2);
-				echo "nama: ";
-				echo $current_name;
-				echo "<br>";
-				echo "email: ";
-				echo $row2['email'];
-				echo "<br>";
-				while($row3 = pg_fetch_row($execute2))
-				{
-					$hari_matkul = $row3[3];
-					echo "hari: ";
-					echo $hari_matkul;
-					echo "<br>";
-					echo "jam mulai: ";
-					$k_mulai = $row3[4];
-					echo $row3[4];
-					echo "<br>";
-					echo "jam selesai: ";
-					$k_selesai= $row3[5];
-					echo $row3[5];
-					echo "<br>";
-					
-					$flag_passed = 0;
-					
-					if($e_mulai < $k_mulai && $e_selesai < $k_mulai)
-					{
-						if($e_mulai < $k_selesai && $e_selesai < $k_selesai)
-						{
-							$flag_passed = 1;
-						}
-					}
-					
-					if($e_mulai > $k_mulai && $e_selesai > $k_mulai)
-					{
-						if($e_mulai > $k_selesai && $e_selesai > $k_selesai)
-						{
-							$flag_passed = 1;
-						}
-					}
-					
-
-				}
-					if($flag_passed == 1)
-					{
-						//block kirim email disini
-						echo $current_name;
-						echo " IS VALID TO SEND";
-						echo "<br>";
-						echo "<br>";
-					}
-				
-				
-			//}
+			$current_user = $row2['id'];
+			//get current user NPM
+			$current_name = $row2['nama'];
+			$current_email = $row2['email'];
+			//get current user name
+			$kueri_row2 = "SELECT a.id, b.nama, b.email, a.hari, a.waktu_mulai, a.waktu_selesai FROM jadwal a RIGHT JOIN user_list b ON a.id = b.id WHERE a.id = '$current_user' AND hari = '$hari_event'";
+			$execute2 = pg_query($kueri_row2);
 			
+			//test block
+			echo "nama: ";
+			echo $current_name;
+			echo "<br>";
+			echo "email: ";
+			echo $row2['email'];
+			echo "<br>";
+			//test block
+			
+			//Loop trough all subject for current user to get time information for every subject, get from JOINED table user_list & jadwal ($kueri_row2)
+			//if true_flag turned 0, then intersect is detected
+			$true_flag = 1;
+			
+			echo "<br>";
+			echo "current true_flag = " . $true_flag;
+			echo "<br>";
+			
+			while($row3 = pg_fetch_row($execute2))
+			{
+				$hari_matkul = $row3[3];
+				//get current subject's day
+				$k_mulai = $row3[4];
+				//get current subject's start time
+				$k_selesai= $row3[5];
+				//get current subject's end time
+				
+				echo "hari: ";
+				echo $hari_matkul;
+				echo "<br>";
+				echo "jam mulai: ";
+				echo $row3[4];
+				echo "<br>";
+				echo "jam selesai: ";
+				echo $row3[5];
+				echo "<br>";
+				
+				//flag to determine if any of the user's subject is intersect with the event's time
+				$flag_passed = 0;
+
+				
+				//the intersect detection logic
+				if($e_mulai < $k_mulai && $e_selesai < $k_mulai)
+				{
+					if($e_mulai < $k_selesai && $e_selesai < $k_selesai)
+					{
+						$flag_passed = 1;
+					}
+					else
+					{
+						$true_flag = 0;
+					}
+						
+				}
+
+				
+				if($e_mulai > $k_mulai && $e_selesai > $k_mulai)
+				{
+					if($e_mulai > $k_selesai && $e_selesai > $k_selesai)
+					{
+						$flag_passed = 1;
+					}
+					else
+					{
+						$true_flag = 0;
+					}
+				}
+
+			}
+	
+			echo "<br>";
+			echo "after check true_flag = " . $true_flag;
+			echo "<br>";
+			
+			//if every checked subject on current user is passing the condition, then send email
+			if($true_flag == 1)
+			{
+				
+				//block test
+				echo $current_name;
+				echo " IS VALID TO SEND";
+				echo "<br>";
+				sendMail($current_email, $current_name, $nama_event, $hari_event, $tanggal_event, $e_mulai_final, $e_selesai_final);
+				echo "<br>";
+				echo "<br>";
+				//block test
+			}
+			else
+			{
+				//block test
+				echo $current_name;
+				echo " IS NOT VALID TO SEND";
+				echo "<br>";
+				echo "<br>";
+				//block test
+			}
 			
 		}
+		//reset the pointer for pg_fetch user & user's subject
 		pg_result_seek($execute,0);
 		pg_result_seek($execute2,0);
 	}
