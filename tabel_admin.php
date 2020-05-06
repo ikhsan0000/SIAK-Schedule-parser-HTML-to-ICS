9fe1fe827e92ee639b7a126ac621289348921bf5
ikhsan0000 <46219884+ikhsan0000@users.noreply.github.com>
2020-05-06 16:27:37 +0700

    Minor Fix
    added code to connect to local database 
    added button for every row

--- tabel_admin.php
+++ tabel_admin.php
@@ -8,12 +8,21 @@ table, th, td {
 </style>
 </head>
 <body>
+<?php
+/* Attempt to connect to database */
+$link = pg_connect("host=localhost dbname=scheduie user=postgres password=120399");
+//check connection
+if($link === false){
+    die("ERROR: Could not connect. " . "Error");
 
+}
+?>
 
 <table style="width:100%">
   <tr>
     <th>Org</th>
     <th>Nama_acara</th>
+	<th>Deskripsi</th>
     <th>Tanggal</th>
     <th>Hari</th>
     <th>waktu_mulai</th>
@@ -26,18 +35,23 @@ table, th, td {
   {
     ?>
     <tr>
-      <td><?php echo $data['Org']; ?></td>
+      <td><?php echo $data['org']; ?></td>
       <td><?php echo $data['nama_acara']; ?></td>
+	  <td><?php echo $data['deskripsi']; ?></td>
       <td><?php echo $data['tanggal']; ?></td>
       <td><?php echo $data['hari']; ?></td>
       <td><?php echo $data['waktu_mulai']; ?></td>
       <td><?php echo $data['waktu_selesai']; ?></td>
-      <td><?php echo $data['sent']; ?></td>
+      <td><?php $data['sent']; 
+	  if($data['sent'] == 0){echo "FALSE";}else{echo "TRUE";}?></td>
+	  <td><button type="button" onclick="alert('Email has been sent!')">Send !</button></td>
     </tr>
     <?php
   }
+
   ?>
 </table><br>
-<button type="button" onclick="alert('Email has been sent!')">Send Email!</button>
+<button type="button" onclick="alert('Email has been sent!')">Send Email to All!</button>
 </body>
 </html>
+
